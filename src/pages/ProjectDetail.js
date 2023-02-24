5dd3e226d57a5dba952072ca9d93fe39257daf36
Uthaeus <romanlavery@gmail.com>
2023-02-24 16:17:08 -0700

    created project detail and project edit pages

---
+++ src/pages/ProjectDetail.js
@@ -0,0 +1,10 @@
+
+
+const ProjectDetailPage = () => {
+    return (
+        <h1>Project Detail Page</h1>
+    );
+};
+
+
+export default ProjectDetailPage;
---
+++ src/pages/ProjectEdit.js
@@ -0,0 +1,11 @@
+
+
+const ProjectEditPage = () => {
+
+    return (
+        <h1>Project Edit Page</h1>
+    );
+};
+
+
+export default ProjectEditPage;
