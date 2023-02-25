98c38844bfdf66dcdcc9f1534ebae8312b45c0ca
Uthaeus <romanlavery@gmail.com>
2023-02-25 14:14:04 -0700

    created About and Contact components with styling

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,29 @@
 
+import classes from '../styles/About.module.css';
 
 const AboutPage = () => {
     return (
-        <h1>About Me</h1>
-    );
+        <div className={classes['about-me']}>
+          <h1>About Me</h1>
+          <img
+            src="https://via.placeholder.com/200x200"
+            alt="Profile"
+            className={classes['profile-picture']}
+          />
+          <p>
+            Hi there! My name is Dude and I'm a video editor with over 5 years
+            of experience. I specialize in creating engaging and visually stunning
+            videos for a wide variety of clients. My work has been featured in
+            several major publications and I've won numerous awards for my
+            contributions to the industry.
+          </p>
+          <p>
+            When I'm not editing videos, you can find me exploring the great
+            outdoors or playing with my dog. I'm also an avid reader and
+            enjoy curling up with a good book on rainy days.
+          </p>
+        </div>
+      );
 };
 
 
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,27 @@
 
+import classes from '../styles/Contact.module.css';
 
 const ContactPage = () => {
     return (
-        <h1>Contact Page</h1>
-    );
+        <div className={classes['contact-form']}>
+          <h1>Contact Me</h1>
+          <form>
+            <div className={classes['form-group']}>
+              <label htmlFor="name">Name</label>
+              <input type="text" id="name" name="name" required />
+            </div>
+            <div className={classes['form-group']}>
+              <label htmlFor="email">Email</label>
+              <input type="email" id="email" name="email" required />
+            </div>
+            <div className={classes['form-group']}>
+              <label htmlFor="message">Message</label>
+              <textarea id="message" name="message" rows="5" required></textarea>
+            </div>
+            <button type="submit">Send</button>
+          </form>
+        </div>
+      );
 };
 
 
