ff33add3c3de7d78ae770e9742cf8984fbce10ae
Uthaeus <romanlavery@gmail.com>
2023-02-26 11:11:55 -0700

    added styling to project item

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,14 +1,16 @@
 import { Link } from "react-router-dom";
 
+import classes from './ProjectItem.module.css';
+
 const ProjectItem = ({project}) => {
     // title, description, videoUrl, thumbnailUrl
 
     return (
-        <article>
+        <article className={classes.project}>
             <img src={project.thumbnailUrl} alt={project.title} />
             <h1>{project.title}</h1>
             <p>{project.description}</p>
-            <menu>
+            <menu className={classes.actions}>
                 <Link to="edit">Edit</Link>
                 <button>Delete</button>
             </menu>
